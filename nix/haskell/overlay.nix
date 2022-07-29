583eb58a9098e5aa412cb9196820e075b20f6920
Markus Barenhoff <mbarenh@alios.org>
2022-07-29 21:23:18 +0200

    made everything compiler with ghc-9.2.4

--- nix/haskell/overlay.nix
+++ nix/haskell/overlay.nix
@@ -11,12 +11,22 @@ let
       # example use a hackage version of a package
       # stylish-haskell = self.callHackage "stylish-haskell" "1.13.0.0" { };
 
-      # example don't run tests
-      # shake = pkgs.haskell.lib.dontCheck (self.callHackage "shake" "0.18.3" {});
+      # hiedb tests fail with 9.2.4 see https://github.com/wz1000/HieDb/issues/46
+      hiedb =
+        pkgs.haskell.lib.dontCheck (self.callHackage "hiedb" "0.4.1.0" { });
+
+      # https://github.com/fourmolu/fourmolu/issues/215
+      fourmolu =
+        pkgs.haskell.lib.dontCheck (self.callHackage "fourmolu" "0.6.0.0" { });
+      ormolu =
+        pkgs.haskell.lib.dontCheck (self.callHackage "ormolu" "0.5.0.0" { });
+
     };
   };
 
-  haskell-language-server = nixpkgsSuper.haskell-language-server.override { supportedGhcVersions = [ "${ghcVersionNumber}" ]; };
+  haskell-language-server = nixpkgsSuper.haskell-language-server.override {
+    supportedGhcVersions = [ "${ghcVersionNumber}" ];
+  };
 
 in {
   haskell = nixpkgsSuper.haskell // {
