d14a0b0a7f90d7668970f544d1a6d63b5d43c025
Markus Barenhoff <mbarenh@alios.org>
2022-07-29 14:30:27 +0200

    merged changes from master

--- nix/haskell/overlay.nix
+++ nix/haskell/overlay.nix
@@ -3,21 +3,20 @@ nixpkgsSelf: nixpkgsSuper:
 let
   inherit (nixpkgsSelf) pkgs;
 
-  ghcVersion = "ghc924";
+  ghcVersionNumber = "924";
+  ghcVersion = "ghc${ghcVersionNumber}";
 
   hsPkgs = nixpkgsSuper.haskell.packages.${ghcVersion}.override {
     overrides = self: super: {
-      # Override ghcide and some of its dependencies since the versions on
-      # Nixpkgs is currently broken.
-
+      # example use a hackage version of a package
       # stylish-haskell = self.callHackage "stylish-haskell" "1.13.0.0" { };
 
-      # compiler bug on aarch64 causes retry test to fail. should be fixed in ghc 9.2.4
-      # see https://github.com/Soostone/retry/issues/78
-      # and https://gitlab.haskell.org/ghc/ghc/-/issues/21624
-      # retry = pkgs.haskell.lib.dontCheck (self.callHackage "retry" "0.9.3.0" {});
+      # example don't run tests
+      # shake = pkgs.haskell.lib.dontCheck (self.callHackage "shake" "0.18.3" {});
     };
   };
+
+  haskell-language-server = nixpkgsSuper.haskell-language-server.override { supportedGhcVersions = [ "${ghcVersionNumber}" ]; };
 
 in {
   haskell = nixpkgsSuper.haskell // {
@@ -25,4 +24,5 @@ in {
 
     packages = nixpkgsSuper.haskell.packages // { "${ghcVersion}" = hsPkgs; };
   };
+  haskell-language-server = haskell-language-server;
 }
